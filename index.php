e76827b320afb9a7c5cc6ff84edd6c7641124014
chairuddinsyah <chairuddinsyah@10.100.8.149>
2016-05-09 18:31:24 +0700

    Initial Commit

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 
-use Aws\S3\S3Client;
-
+require '/var/www/vendor/autoload.php';
 include "/var/www/awss3_dev.php";
+
+use Aws\S3\S3Client;
 
 // Instantiate the S3 client with your AWS credentials
 $client = S3Client::factory(array(
