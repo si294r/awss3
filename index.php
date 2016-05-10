e7a2cd297ea1075c6c9fd21a2e99b74a60abe628
chairuddinsyah <chairuddinsyah@10.100.8.149>
2016-05-10 09:39:21 +0700

    Update Commit

--- index.php
+++ index.php
@@ -15,4 +15,24 @@ $client = S3Client::factory(array(
 
 //var_dump($client);
 $result = $client->listBuckets();
-print_r($result);
+var_dump($result);
+
+die;
+
+// Upload an object by streaming the contents of a file
+// $pathToFile should be absolute path to a file on disk
+$result = $client->putObject(array(
+    'Bucket'     => $bucket,
+    'Key'        => 'data_from_file.txt',
+    'SourceFile' => $pathToFile,
+    'Metadata'   => array(
+        'Foo' => 'abc',
+        'Baz' => '123'
+    )
+));
+
+// We can poll the object until it is accessible
+$client->waitUntil('ObjectExists', array(
+    'Bucket' => $this->bucket,
+    'Key'    => 'data_from_file.txt'
+));
