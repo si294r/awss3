4ee10d0d1f16ca8345cce4cca389e6ed7c44b2b3
chairuddinsyah <chairuddinsyah@10.100.8.149>
2016-05-16 17:26:06 +0700

    Update Commit

--- index.php
+++ index.php
@@ -10,31 +10,6 @@ function get_url_download($value) {
     return "http://files.parsetfss.com/e1a9cd04-e62a-4463-8524-177866bb62e6/" . $value;
 }
 
-$connection_string = "mongodb://"
-        . $mongo_username . ":"
-        . $mongo_password . "@"
-        . $mongo_host . "/"
-        . $mongo_database;
-
-$client = new MongoDB\Client($connection_string, $mongo_options); // create object client 
-
-$db = $client->$mongo_database; // select database
-//$db->setSlaveOkay();
-
-$document = $db->selectCollection('_User')->findOne(['cloudSaveDataAndroid' => [ '$exists' => TRUE ]]);
-
-$url_download = get_url_download($document["cloudSaveDataAndroid"]);
-var_dump($url_download);
-die;
-
-$clientS3 = S3Client::factory(array(
-            'credentials' => array(
-                'key' => $aws_key,
-                'secret' => $aws_secret_key
-            )
-        ));
-
-
 function upload_file_s3($pathToFile) {
     global $clientS3;
     global $aws_bucket; // defined in include file awss3
@@ -54,3 +29,32 @@ function upload_file_s3($pathToFile) {
         'Key' => $filename
     ));
 }
+
+$connection_string = "mongodb://"
+        . $mongo_username . ":"
+        . $mongo_password . "@"
+        . $mongo_host . "/"
+        . $mongo_database;
+
+$client = new MongoDB\Client($connection_string, $mongo_options); // create object client 
+
+$db = $client->$mongo_database; // select database
+//$db->setSlaveOkay();
+
+$document = $db->selectCollection('_User')->findOne(['cloudSaveDataAndroid' => [ '$exists' => TRUE ]]);
+
+$url_download = get_url_download($document["cloudSaveDataAndroid"]);
+//var_dump($url_download);
+//die;
+
+exec("wget ".$url_download);
+
+$clientS3 = S3Client::factory(array(
+            'credentials' => array(
+                'key' => $aws_key,
+                'secret' => $aws_secret_key
+            )
+        ));
+
+upload_file_s3($document["cloudSaveDataAndroid"]);
+
